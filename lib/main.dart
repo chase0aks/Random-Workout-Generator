3c002cfd3356488854d34c146c1d4c4d49a26b50
Chase Oaks <chaseoaks14@gmail.com>
2023-01-30 11:22:56 -0600

    Finished_Learning

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'app_project',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
