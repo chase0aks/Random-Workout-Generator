c29fde0997f598bd7e13f8a376858a84cab6c965
Chase Oaks <chaseoaks14@gmail.com>
2023-01-31 23:48:43 -0600

    I think the web icons need updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+//to commit//
+
 void main() {
   runApp(MyApp());
 }
