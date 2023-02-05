79cccd463215c7279d80da3293c784baa59abccf
Coaks <Coaks@CHASES>
2023-02-04 21:19:14 -0600

    added non functioning sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -65,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = FavoritesPage();
         break;
       case 2:
-        page = NahPage();
+        page = LoginPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -212,30 +212,128 @@ class FavoritesPage extends StatelessWidget {
   }
 }
 
-class NahPage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          leading:
+          IconButton( onPressed: (){
+            Navigator.pop(context);
+          },icon:Icon(Icons.arrow_back_ios,size: 20,color: Colors.black,)),
         ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Column(
+                  children: [
+                    Text ("Login", style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    SizedBox(height: 20,),
+                    Text("Welcome back ! Login with your credentials",style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[700],
+                    ),),
+                    SizedBox(height: 30,)
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40
+                  ),
+                  child: Column(
+                    children: [
+                      makeInput(label: "Email"),
+                      makeInput(label: "Password",obsureText: true),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 3,left: 3),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black)
+                        )
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height:60,
+                      onPressed: (){},
+                      color: Colors.indigoAccent[400],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40)
+                      ),
+                      child: Text("Login",style: TextStyle(
+                        fontWeight: FontWeight.w600,fontSize: 16,color: Colors.white70
+                      ),),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Dont have an account?"),
+                    Text("Sign Up",style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18
+                    ),),
+                  ],
+                )
+              ],
+
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget makeInput({label,obsureText = false}){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(label,style:TextStyle(
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        color: Colors.black87
+      ),),
+      SizedBox(height: 5,),
+      TextField(
+        obscureText: obsureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.deepOrange,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange)
+          ),
+        ),
+      ),
+      SizedBox(height: 30,)
+
+    ],
+  );
 }
